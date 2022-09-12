6017319ed88b8b0e7d3034f4af679b69b80cad29
Mona Jha <mona.jha@inindcrws.com>
2022-09-12 21:55:27 +0530

    added comment

--- src/main/java/login/LoginClass.java
+++ src/main/java/login/LoginClass.java
@@ -1,7 +1,7 @@
 package login;
 
 public class LoginClass {
-	
+	//all data
 	 public static final String baseURL="https://classic.crmpro.com";
 	 public static final String Username="groupautomation";
 	 public static final String Password="Test@12345";
